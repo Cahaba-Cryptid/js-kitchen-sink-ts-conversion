83fa2988010dc673f5d43856896e63ee7959b45c
Hayden <chfrank9011@gmail.com>
2019-04-25 15:16:20 -0500

    finishing up changes

--- app.js
+++ app.js
@@ -1,67 +1,56 @@
+"use strict";
 //Defines myName as 'Hayden"
-let myName = 'Hayden';
+var myName = 'Hayden';
 //Give a constant for the number of US states.
-const statesoftheUS = '50 states';
-
+var statesoftheUS = '50 states';
 //The below variable adds 5 and 4.
-let val = 5 + 4;
-
+var val = 5 + 4;
 //Presents an alert box that must be clicked in order to continue.
-function sayHello() {
+var sayHello = function () {
     alert('Hello World!');
-}
-
+};
 sayHello();
-
-function checkAge(name, age) {
-    if (age < 21) {
-        alert('Sorry ' + name + ', you are not old enough to view this page!');
-    }
-}
-
+var checkAge = function (name, age) {
+    if (age < 21)
+        alert("Sorry " + name + ", you are not old enough to view this page!");
+};
 checkAge('Charles', 21);
 checkAge('Abby', 27);
 checkAge('James', 18);
 checkAge('John', 17);
-
-let favVeg = ['brussels sprouts', 'yellow onion', 'broccoli', 'lima beans', 'spinach', 'arugula'];
-
-for (let x = 0; x < favVeg.length; x++) {
+var favVeg = ['brussels sprouts', 'yellow onion', 'broccoli', 'lima beans', 'spinach', 'arugula'];
+for (var x = 0; x < favVeg.length; x++) {
     console.log(favVeg[x]);
-};
-
-let friends = [{
-    name: 'Katie',
-    age: 25
-}, {
-    name: 'Paul',
-    age: 27
-}, {
-    name: 'Greg',
-    age: 19
-}, {
-    name: 'Sally',
-    age: 20
-}, {
-    name: 'Pat',
-    age: 23
-}];
-
-for (let x = 0; x < friends.length; x++) {
+}
+;
+var friends = [{
+        name: 'Katie',
+        age: 25
+    }, {
+        name: 'Paul',
+        age: 27
+    }, {
+        name: 'Greg',
+        age: 19
+    }, {
+        name: 'Sally',
+        age: 20
+    }, {
+        name: 'Pat',
+        age: 23
+    }];
+for (var x = 0; x < friends.length; x++) {
     checkAge(friends[x].name, friends[x].age);
 }
 //Make sure the argument is des
-function getLength(arg) {
+var getLength = function (arg) {
     return arg.length;
-}
-
-let wordLength = getLength('Hello World');
-
+};
+var wordLength = getLength('Hello World');
 console.log(wordLength);
-
 if (wordLength % 2 == 0) {
     console.log("The world is nice and even!");
-} else {
+}
+else {
     console.log("The world is an odd place!");
 }
-
---
+++ app.ts
@@ -0,0 +1,67 @@
+
+
+//Defines myName as 'Hayden"
+let myName = 'Hayden';
+//Give a constant for the number of US states.
+const statesoftheUS = '50 states';
+
+//The below variable adds 5 and 4.
+let val = 5 + 4;
+
+//Presents an alert box that must be clicked in order to continue.
+let sayHello = () => {
+    alert('Hello World!');
+}
+
+sayHello();
+
+let checkAge = (name: string, age: number) => {
+    if (age < 21) alert(`Sorry ${name}, you are not old enough to view this page!`);
+}
+
+checkAge('Charles', 21);
+checkAge('Abby', 27);
+checkAge('James', 18);
+checkAge('John', 17);
+
+let favVeg = ['brussels sprouts', 'yellow onion', 'broccoli', 'lima beans', 'spinach', 'arugula'];
+
+for (let x = 0; x < favVeg.length; x++) {
+    console.log(favVeg[x]);
+};
+
+let friends = [{
+    name: 'Katie',
+    age: 25
+}, {
+    name: 'Paul',
+    age: 27
+}, {
+    name: 'Greg',
+    age: 19
+}, {
+    name: 'Sally',
+    age: 20
+}, {
+    name: 'Pat',
+    age: 23
+}];
+
+for (let x = 0; x < friends.length; x++) {
+    checkAge(friends[x].name, friends[x].age);
+}
+//Make sure the argument is des
+let getLength = (arg: string) => {
+    return arg.length;
+}
+
+let wordLength = getLength('Hello World');
+
+console.log(wordLength);
+
+if (wordLength % 2 == 0) {
+    console.log("The world is nice and even!");
+} else {
+    console.log("The world is an odd place!");
+}
+
